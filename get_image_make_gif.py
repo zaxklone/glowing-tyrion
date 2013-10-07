8727c76c4c3f022e8e693ee1ca5837796c3cd419
zaxklone <zaxklone@zaxklone-desktop.(none)>
2013-10-07 21:56:06 +0200

    Completed script

--- get_image_make_gif.py
+++ get_image_make_gif.py
@@ -1,7 +1,27 @@
 #! /usr/bin/python 
+# Retrieves images from the forthcoming image generation app that is currently
+# located at http://localhost:8000/ 
+# 
+
+
+import urllib2
+import urllib
+#for retrieving images and all things http
+
+SERVER = "http://localhost:8080"
+
+
+
 
 def main():
-    print 'test'
-
+    for i in range(100):
+        try:
+            img_name = "./images/test{0}.png".format(str(i).zfill(3))
+            response =  urllib.urlretrieve(SERVER,img_name )
+        except:
+            print 'error'
+    #print response.getcode()
+    #print response.read()
+        
 if __name__ == '__main__':
     main()
