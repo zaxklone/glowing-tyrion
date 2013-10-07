9a1bdee7cb68ce456c9fbe06124dc3ff8d688330
zaxklone <zaxklone@zaxklone-desktop.(none)>
2013-10-07 19:49:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+My Scripts
+##########
+
+1. get_image_make_gif.py
+   
+  - Gets images and makes a gif.
+
+
---
+++ get_image_make_gif.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python 
+
+def main():
+    print 'test'
+
+if __name__ == '__main__':
+    main()
